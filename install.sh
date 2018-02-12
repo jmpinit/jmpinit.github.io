61df737cbc01a416619e83ba8197b9100273fb4b
Owen Trueblood <hi@owentrueblood.com>
2018-02-12 15:12:28 -0500

    Explain changes before executing them

--- install.sh
+++ install.sh
@@ -10,20 +10,16 @@ function finish {
 
 # Ensure that Vim is installed
 command -v vim >/dev/null 2>&1 || {
-  read -p "Vim is not installed. Install it (y/n)? " -n 1 -r
-  echo # New line
-  if [[ $REPLY =~ ^[Yy]$ ]]
-  then
-    sudo apt-get install vim
-  else
-    finish
-  fi
+  echo "Vim is not installed. Installing it now."
+  sudo apt-get install vim
 }
 
 # Overwrite .vimrc
+echo "Overwriting ~/.vimrc."
 curl -L https://raw.githubusercontent.com/jmptable/jmptable.github.io/master/dotfiles/.vimrc > $HOME/.vimrc
 
 # Set vim as system default editor
+echo "Setting Vim as the default editor."
 update-alternatives --set editor /usr/bin/vim.basic
 
 finish
