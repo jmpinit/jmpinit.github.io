34dd18e1064a98057e6f37f00df3d9f50b19e060
Owen Trueblood <hi@owentrueblood.com>
2018-02-20 23:33:43 -0500

    Accept installation automatically

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ function finish {
 # Ensure that Vim is installed
 command -v vim >/dev/null 2>&1 || {
   echo "Vim is not installed. Installing it now."
-  sudo apt-get install vim
+  sudo apt-get install -y vim
 }
 
 # Overwrite .vimrc
@@ -24,7 +24,7 @@ sudo update-alternatives --set editor /usr/bin/vim.basic
 
 # Install zsh
 echo "Installing zsh and oh-my-zsh."
-sudo apt-get install zsh
+sudo apt-get install -y zsh
 sudo -E sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 finish
