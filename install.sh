5c1dd887b3619042f2de4c736a3562d10573144b
Owen Trueblood <hi@owentrueblood.com>
2018-09-19 15:52:38 -0400

    Multiplatform package installation

--- install.sh
+++ install.sh
@@ -22,6 +22,27 @@ function finish {
 
 trap finish EXIT
 
+function install_package {
+  # Update package listings once before installing any packages
+  if [ -z ${PACKAGES_UPDATED+x} ]; then
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+      sudo apt-get update --fix-missing
+    elif [[ "$OSTYPE" == "darwin" ]]; then
+      brew update
+    fi
+
+    PACKAGES_UPDATED=1
+  fi
+
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    sudo apt-get install -y $1
+  elif [[ "$OSTYPE" == "darwin" ]]; then
+    brew install $1
+  else
+    echo "Cannot install $1 on this platform"
+  fi
+}
+
 function configure_vim {
   # Overwrite .vimrc
   echo "Overwriting ~/.vimrc."
@@ -41,24 +62,27 @@ function set_vim_as_default {
 function install_zsh {
   # Install zsh
   echo "Installing zsh and oh-my-zsh."
-  sudo apt-get install -y zsh
+  install_package zsh
   sudo -E sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
 
 function install_common {
+  install_package git
+  install_package vim
+  
   configure_vim
   set_vim_as_default
-}
-
-function install_linux {
-  sudo apt-get update --fix-missing
-  sudo apt-get install -y git vim
   install_zsh
 }
 
-function install_windows {
+function install_linux {
   # Nothing yet
   echo
+}
+
+function install_windows {
+  echo "Install SharpKeys from https://github.com/randyrants/sharpkeys/releases"
+  echo "Run SharpKeys and swap escape with caps lock"
 }
 
 install_common
